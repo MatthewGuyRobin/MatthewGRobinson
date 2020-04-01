e00714f313662ff863dc44f2e8ca73cac2b4830c
MatthewGuyRobin <matthewguyrobinson@gmail.com>
2020-04-01 19:48:25 +0100

    document title function added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import React, { Component } from 'react'
-// import Header from "./components/Header";
-// import Quote from "./components/Quote";
-// import Currentwork from "./components/Currentwork";
-// import Journey from './components/Journey';
-// import Recent from './components/Recent';
-// import Skills from './components/Skills';
 import Home from "./components/Home";
 import Links from "./components/Links";
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
@@ -13,10 +7,14 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 export default class App extends Component {
 
 
+
+  componentDidMount() {
+    document.title = "Matthew Guy Robinson";
+}
+
   render() {
     return (
       <div>
-      {/* <Home/> */}
       <BrowserRouter>
               <Switch>
                   <Route path="/" component={Home} exact/>
