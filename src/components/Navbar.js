bbd734bbfa1cbff9b27b0756f85339e604924efc
MatthewGuyRobin <matthewguyrobinson@gmail.com>
2020-04-02 16:56:01 +0100

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-            <nav className="navcontainer">
-                <ul className="navtext">
-                    <li>
-                        <Link className="navbord" to="/MatthewGRobinson">Home</Link>
-                    </li>
-                    <li>
-                        <Link className="navbord" to="/Success">Achievements</Link>
-                    </li>
-                </ul>
-            </nav>
-    
+        <nav className="navcontainer">
+            <ul className="navtext">
+                <li>
+                    <Link className="navbord" to="/MatthewGRobinson">Home</Link>
+                </li>
+                <li>
+                    <Link className="navbord" to="/Success">Achievements</Link>
+                </li>
+            </ul>
+        </nav>
+
     )
 }
 export default Navbar;
