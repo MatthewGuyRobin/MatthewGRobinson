92214ef0bfa7653f90a2cf37050a2ac176a06c5a
MatthewGuyRobin <matthewguyrobinson@gmail.com>
2020-04-02 12:02:41 +0100

    correct comments for right project displayed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Home from "./components/Home";
 import Links from "./components/Links";
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 
 export default class App extends Component {
@@ -10,17 +10,17 @@ export default class App extends Component {
 
   componentDidMount() {
     document.title = "Matthew Guy Robinson";
-}
+  }
 
   render() {
     return (
       <div>
-      <BrowserRouter>
-              <Switch>
-                  <Route path="/" component={Home} exact/>
-                  <Route path="/links" component={Links}/>
-              </Switch>
-          </BrowserRouter>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/links" component={Links} />
+          </Switch>
+        </BrowserRouter>
       </div>
     )
   }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import chuck from '../img/SpaceXScreenShot.png'
+import spacex from '../img/SpaceXScreenShot.png'
 
 export default function Skills() {
     return (
         <div className="api-container">
             <h3 className="react-text title">SpaceX API</h3>
             <div className="screenshot">
-                <img className="image" src={chuck} alt="chucknorrisscreenshot" />
-                <p>On this project using the React library I have created a button so that on click a new Chuck Norris joke is generated. Created a state with an array called data that is empty. This has been done so that when the function componentDidMount is run it fetches one joke from the url, changes this to json data and we use setState to update the value in the empty array.</p>
+                <img className="image" src={spacex} alt="spacexformapi" />
+                <p>This is a project in which on user input the details of spaceX missions past and future are displayed. This has been done by using Express, Handle-bars & bodyParser. Using Express I was able to obtain the information from the API & then show details of the flight that had been requested using Handle bars. </p>
             </div>
         </div>
     )
